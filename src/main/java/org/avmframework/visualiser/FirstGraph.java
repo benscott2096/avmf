862a3afdcb3d6af9fab3aee309c04ce67a9b5246
benscott2096 <benscott2096@gmail.com>
2019-04-12 18:09:36 +0100

    Animation control finished with jumping backwards and forwards between variables implemented. General tidyup and commenting.

--- src/main/java/org/avmframework/visualiser/FirstGraph.java
+++ src/main/java/org/avmframework/visualiser/FirstGraph.java
@@ -50,15 +50,21 @@ enum YDirection{
 
 public class FirstGraph extends Application {
 
+    // to keep a record of what the original upper and lower bounds were calculated as using JavaFx bulit-in autoranging.
     private double originalXAxisLowerBound, originalXAxisUpperBound;
     private double originalYAxisLowerBound, originalYAxisUpperBound;
 
+    // to store values involved in calculating logarithmic axes.
     private double xALog, xBLog, yALog, yBLog;
 
+    // used to keep track of what variable is having its series set up.
     private int currentVariable = 1;
+    // variable to keeping track of which vector variable is currently being animated
+    private int currentAnimatedVariable = 0; // first landscape cuepoint has label 0
+    private int noOfVariables = start.getDataPairs().get(0).getVector().size();
 
-//    Timeline timeline = new Timeline();
-    SequentialTransition sequence = new SequentialTransition();
+    // the avmfAnimationSequence
+    private SequentialTransition avmfAnimationSequence = new SequentialTransition();
 
     @Override public void start(Stage stage) {
         stage.setTitle("Line Chart Sample");
@@ -175,30 +181,31 @@ public class FirstGraph extends Application {
         Button playAnimationButton = new Button("Play Animation");
         playAnimationButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
-                sequence.play();
+                avmfAnimationSequence.play();
+                System.out.println(avmfAnimationSequence.getCuePoints());
             }
         });
 
         Button pauseAnimationButton  = new Button("Pause Animation");
         pauseAnimationButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
-                sequence.pause();
+                avmfAnimationSequence.pause();
             }
         });
 
         Button restartAnimationButton  = new Button("Restart Animation");
         restartAnimationButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
-                sequence.playFromStart();
+                avmfAnimationSequence.playFromStart();
             }
         });
 
         Button decreaseRateButton  = new Button("Decrease Rate");
         decreaseRateButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
-                double currentRate = sequence.getCurrentRate();
+                double currentRate = avmfAnimationSequence.getCurrentRate();
                 if (currentRate > 1) {
-                    sequence.setRate(currentRate - 1);
+                    avmfAnimationSequence.setRate(currentRate - 1);
                 }
             }
         });
@@ -206,9 +213,9 @@ public class FirstGraph extends Application {
         Button increaseRateButton  = new Button("Increase Rate");
         increaseRateButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
-                double currentRate = sequence.getCurrentRate();
+                double currentRate = avmfAnimationSequence.getCurrentRate();
                 if (currentRate <= 9) {
-                    sequence.setRate(currentRate + 1);
+                    avmfAnimationSequence.setRate(currentRate + 1);
                 }
             }
         });
@@ -216,17 +223,37 @@ public class FirstGraph extends Application {
         Button jumpToEndButton  = new Button("Jump to end");
         jumpToEndButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
-            sequence.jumpTo("end");
+            avmfAnimationSequence.jumpTo("end");
             }
         });
 
 
-//        Button jumpToNextVarButton  = new Button("Jump to next variable");
-//        jumpToNextVarButton.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override public void handle(ActionEvent e) {
-//                sequence.jumpTo();
-//            }
-//        });
+        Button previousVariableButton  = new Button("previous variable");
+        previousVariableButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                if (currentAnimatedVariable > 0) {
+                    currentAnimatedVariable--;
+                }
+                System.out.println("currentAnimatedVariable: " + currentAnimatedVariable); // debugging
+                avmfAnimationSequence.jumpTo(String.valueOf(currentAnimatedVariable));
+
+            }
+        });
+
+        Button nextVariableButton  = new Button("next variable");
+        nextVariableButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                // if possible increment index of current animated variable
+                if (currentAnimatedVariable < noOfVariables - 1) {
+                    currentAnimatedVariable++;
+                }
+                System.out.println("currentAnimatedVariable: " + currentAnimatedVariable); // debugging
+                // jump to cure point of next variable.
+                avmfAnimationSequence.jumpTo(String.valueOf(currentAnimatedVariable));
+
+
+            }
+        });
 
 
 
@@ -249,6 +276,9 @@ public class FirstGraph extends Application {
         layout.getChildren().add(decreaseRateButton);
         layout.getChildren().add(increaseRateButton);
         layout.getChildren().add(jumpToEndButton);
+        layout.getChildren().add(previousVariableButton);
+        layout.getChildren().add(nextVariableButton);
+
 
 
         Scene scene  = new Scene(layout,800,600);
@@ -259,70 +289,71 @@ public class FirstGraph extends Application {
 
 
 
-        // all this happening after stage.show() because autoranging done at this point and is needed to set default values in the slider.
+        // all this happening after stage.show() because autoranging has been done by this point and is needed to set default values in the slider.
         final NumberAxis xAxis = (NumberAxis) lineChart.getXAxis();
 
+        // setting original x axis bounds from autoranging
         originalXAxisLowerBound = xAxis.getLowerBound();
         originalXAxisUpperBound = xAxis.getUpperBound();
 
+        // debugging
         System.out.println("original X lower bound");
         System.out.println(originalXAxisLowerBound);
-
         System.out.println("original X upper bound");
         System.out.println(originalXAxisUpperBound);
 
-
+        // setting up logarithm calculation engine for x axis zooming
         xSetupLogCalc(0.1, originalXAxisUpperBound);
-
+        // setting x zoom slider default values
         xZoomSlider.setMax(originalXAxisUpperBound);
         xZoomSlider.setValue(originalXAxisUpperBound);
-        xZoomSlider.setMajorTickUnit(originalXAxisUpperBound/4);
-
-
-
-
+        xZoomSlider.setMajorTickUnit(originalXAxisUpperBound); // line to fudge the showing of no major tick marks. Can't easily show them because they don't correspond to logarithmic zoom
 
 
 
         final NumberAxis yAxis = (NumberAxis) lineChart.getYAxis();
 
+        // setting original y axis bounds from autoranging
         originalYAxisLowerBound = yAxis.getLowerBound();
         originalYAxisUpperBound = yAxis.getUpperBound();
 
+        // debugging
         System.out.println("original Y lower bound");
         System.out.println(originalYAxisLowerBound);
-
         System.out.println("original Y upper bound");
         System.out.println(originalYAxisUpperBound);
 
 
+        // setting up logarithm calculation engine for y axis zooming
         ySetupLogCalc(0.1, originalYAxisUpperBound);
-
+        // setting y zoom slider default values
         yZoomSlider.setMax(originalYAxisUpperBound);
         yZoomSlider.setValue(originalYAxisUpperBound);
-        yZoomSlider.setMajorTickUnit(originalYAxisUpperBound/4);
+        yZoomSlider.setMajorTickUnit(originalYAxisUpperBound);// line to fudge the showing of no major tick marks. Can't easily show them because they don't correspond to logarithmic zoom
 
 
     }
 
 
-
+    // takes in a value between x axis upper and lower bounds (value used from slider) and returns its logarithmic zoom equivalent
     private double xCalcLogValue(double value){
         double logValue = xALog * Math.exp(xBLog *value);
         return logValue;
     }
 
+    // method to calculate and store values needed for logarithmic x axis zoom calculations
     private void xSetupLogCalc(double x, double y){
         xBLog = Math.log(y/x)/(y-x);
         xALog = y / Math.exp(xBLog * y);
     }
 
-
+    // takes in a value between y axis upper and lower bounds (value used from slider) and returns its logarithmic zoom equivalent
     private double yCalcLogValue(double value){
         double logValue = yALog * Math.exp(yBLog *value);
         return logValue;
     }
 
+    // method to calculate and store values needed for logarithmic y axis zoom calculations
     private void ySetupLogCalc(double x, double y){
         yBLog = Math.log(y/x)/(y-x);
         yALog = y / Math.exp(yBLog * y);
@@ -340,8 +371,11 @@ public class FirstGraph extends Application {
     }
 
 
-
+    // method that returns a line chart set up with the series of all vector variables.
     private LineChart<Number,Number> makeLineChart(ArrayList<AvmfIterationOutput> dataPairs) {
+
+        double runningDurationTotal = 0.0;
+        int previousVarCount = 0;
 
         //defining the axes
         final NumberAxis xAxis = new NumberAxis();
@@ -358,42 +392,69 @@ public class FirstGraph extends Application {
 
 
 
-
-
-
         // setting up and adding series for all vector variables to chart? also adding animation keyframes.
-        for (int it = 0; it < dataPairs.get(0).getVector().size(); it++) {
+        for (int variableNo = 0; variableNo < dataPairs.get(0).getVector().size(); variableNo++) {
+            // setting up series
             XYChart.Series series = setUpSeries(dataPairs);
             lineChart.getData().add(series);
+            // setting series line (represents landscape) to transparent
             Node seriesNode = series.getNode();
             seriesNode.setOpacity(0);
 
+            // animation speed control constants.
+            final double STANDARD_POINT_ANIMATION_TIME = 1000; // in milliseconds
+            final double STANDARD_LANDSCAPE_ANIMATION_TIME = 2000; // in milliseconds
 
+            // setting up animation for series line fade in.
             Timeline fadeInLandscape = new Timeline();
-//            KeyFrame key = new KeyFrame(new Duration(2000), new KeyValue(seriesNode.opacityProperty(), 1));
-//            key.
-
             fadeInLandscape.getKeyFrames().addAll(
-                    new KeyFrame(new Duration(2000), String.valueOf(it),  new KeyValue(seriesNode.opacityProperty(), 1))
+                    new KeyFrame(new Duration(STANDARD_LANDSCAPE_ANIMATION_TIME), "landscape of Variable " + String.valueOf(variableNo),  new KeyValue(seriesNode.opacityProperty(), 1))
             );
-            sequence.getChildren().add(fadeInLandscape);
+            final int variableNoHelper = variableNo; // need new final variable declaration to get variableNo into inner class
+            // event handler to update the global variable keeping track of which vector variable is currently being animated
+            fadeInLandscape.setOnFinished(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    currentAnimatedVariable = variableNoHelper;
+                }
+            });
+            avmfAnimationSequence.getChildren().add(fadeInLandscape); // adding timeline fade to avmfAnimationSequence
 
 
-            ObservableList<XYChart.Data> theData = series.getData();
+
             // setting up animation timeline for dropping points onto landscape.
+            ObservableList<XYChart.Data> theData = series.getData();
             for (XYChart.Data dataPoint : theData){
                 Node dataPointNode = dataPoint.getNode();
                 dataPointNode.setOpacity(0);
                 dataPointNode.setScaleX(4);
                 dataPointNode.setScaleY(4);
                 Timeline dropPoint = new Timeline();
+                // keyframes for doing the fade in and shrink onto landscape for each point.
                 dropPoint.getKeyFrames().addAll(
                         new KeyFrame(new Duration(1000), new KeyValue(dataPointNode.opacityProperty(), 1 )),
                         new KeyFrame(new Duration(1000), new KeyValue(dataPointNode.scaleXProperty(), 1 )),
                         new KeyFrame(new Duration(1000), new KeyValue(dataPointNode.scaleYProperty(), 1 ))
                         );
-                sequence.getChildren().addAll(dropPoint);
+                avmfAnimationSequence.getChildren().addAll(dropPoint); // adding keyframes to avmfAnimationSequence.
+
             }
+            System.out.println("series.length : " + theData.size());
+
+            // conditional allows the first cue point to = 0 milliseconds
+            if (variableNo > 0){
+                double varCount = previousVarCount; // explicitly coded fix for indexing issue where cue points were set off by 1.
+                double thisVarDuration = varCount * STANDARD_POINT_ANIMATION_TIME; // length of this variables duration with offset from landscape fades.
+                double thisVarStartTime = runningDurationTotal + thisVarDuration + STANDARD_LANDSCAPE_ANIMATION_TIME; // calculate start time of this variableNo
+                Duration nextVarStartDuration = new Duration(thisVarStartTime); // new duration object with the this variables start time.
+                avmfAnimationSequence.getCuePoints().put(String.valueOf(variableNo), nextVarStartDuration); // add cue point to avmfAnimationSequence.
+                runningDurationTotal = thisVarStartTime; // update running total
+            }
+            else if (variableNo == 0){
+                avmfAnimationSequence.getCuePoints().put(String.valueOf(variableNo), new Duration(0));
+            }
+            previousVarCount = theData.size(); // update for fixing indexing issue
+
         }
 
 
@@ -501,7 +562,6 @@ private void panGraph(LineChart<Number,Number> lineChart, XDirection xDirection,
          yPanStep = yWidth / (PAN_SENSITIVITY); // still seems to sensitive at higher zooms.
     }
 
-
     // X axis panning right
     if(xCurrentUpperBound < originalXAxisUpperBound){
         if (xDirection == XDirection.RIGHT){
@@ -510,7 +570,7 @@ private void panGraph(LineChart<Number,Number> lineChart, XDirection xDirection,
         }
     }
     else{
-        System.out.println("reached X upper bound");
+        System.out.println("reached X upper bound"); // terminal reporting/debugging
     }
     // X axis panning Left
     if(xCurrentLowerBound > originalXAxisLowerBound){
@@ -520,7 +580,7 @@ private void panGraph(LineChart<Number,Number> lineChart, XDirection xDirection,
         }
     }
     else {
-        System.out.println("reached X Lower bound");
+        System.out.println("reached X Lower bound"); // terminal reporting/debugging
     }
 
     // Y axis panning down
@@ -531,7 +591,7 @@ private void panGraph(LineChart<Number,Number> lineChart, XDirection xDirection,
         }
     }
     else{
-        System.out.println("reached Y upper bound");
+        System.out.println("reached Y upper bound"); // terminal reporting/debugging
     }
     // Y axis panning up
     if(yCurrentLowerBound > originalYAxisLowerBound){
@@ -541,7 +601,7 @@ private void panGraph(LineChart<Number,Number> lineChart, XDirection xDirection,
         }
     }
     else {
-        System.out.println("reached Y Lower bound");
+        System.out.println("reached Y Lower bound"); // terminal reporting/debugging
     }
 
 
@@ -561,35 +621,14 @@ private void panGraph(LineChart<Number,Number> lineChart, XDirection xDirection,
                 ObservableList seriesData = series.getData();
                 // add the data point to series
                 seriesData.add(new XYChart.Data(pair.getVector().get(currentVariable -1), pair.getObjVal()));
-                // set data point ... visiblity proprty
-////                double x,y;
-////                XYChart.Data<Number,Number> dataPoint = seriesData.get(seriesData.size() - 1);
-//                System.out.println("seriesData: " + seriesData.get(0).getNode());
-
-
-
-
-
             }
         }
-
-
 
 
         currentVariable++;
         return series;
     }
 
-    // why do I have this?
-    public void xZoom(LineChart<Number,Number> lineChart){
-        final NumberAxis xAxis = (NumberAxis) lineChart.getXAxis();
-        System.out.println(xAxis.getLowerBound());
-
-        xAxis.setLowerBound(-10);
-        xAxis.setUpperBound(10);
-
-
-    }
 
     // for zooming x axis
     public void xZoom(LineChart<Number,Number> lineChart, double lowerBound, double upperBound){
